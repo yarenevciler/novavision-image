5921d8feb12990c0b58ade0f89bc826625b1b03d
Yaren Evciler <evcileryaren1@gmail.com>
2023-08-02 17:07:26 +0300

    Post an API Request Using Postman and Save Output

--- service.py
+++ service.py
@@ -5,7 +5,7 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '../'))
 import uvicorn
 from fastapi import FastAPI,Request
-
+#from pydantic import BaseModel
 import json
 
 from capsules.capTrafficSignClassifier.src.executors.TrafficSign import TrafficInferrer
@@ -19,7 +19,16 @@ executors = {'Traffic':{"Traffic":TrafficInferrer}}
 btstrp = Bootstrap(executors)
 bootstrap = btstrp.run()
 
-
+#class JsonPayload(BaseModel):
+    #data :dict
+#@app.post("/receive")
+#def receive_json(payload: JsonPayload):
+    #try:
+        #with open("received_payload.json","w") as json_file:
+            #json.dump(payload.data, json_file, indent = 4)
+        #return{"message":"JSON verisi kaydedildi."}
+    #except Exception as e:
+        #raise HTTPException(status_code = 500, detail= str(e))
 
 @app.post('/api')
 async def api(request: Request):
